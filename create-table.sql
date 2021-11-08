abb7a5392acff36431d64e6888197aa57d5df3b6
Jcloux <joseph.cloux@ynov.com>
2021-11-08 09:48:02 +0100

    v1

---
+++ create-table.sql
@@ -0,0 +1,15 @@
+-- Create a table students which contains in order 5 columns :
+
+-- StudentId with type INTEGER / auto-increment and primary key, not nullable
+-- FirstName with type VARCHAR(80) / not nullable
+-- LastName with type VARCHAR(80) / not nullable
+-- Birthday with type DATE / not nullable
+-- City with type VARCHAR(50) / not nullable
+
+CREATE TABLE students (
+  StudentId INT PRIMARY KEY,
+  FirstName VARCHAR NOT NULL,
+  LastName VARCHAR NOT NULL,
+  Birthday DATE NOT NULL,
+  City VARCHAR NOT NULL
+);
