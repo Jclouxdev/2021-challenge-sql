ec8d6232a9cdf068677edb8631e193d7be738ce0
Jcloux <joseph.cloux@ynov.com>
2021-11-08 17:43:18 +0100

    fix

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -1,6 +1,5 @@
-SELECT DISTINCT tracks.Name AS TrackName, playlists.Name AS PlaylistName
-FROM tracks
-INNER JOIN playlist_track ON tracks.TrackId = playlist_track.TrackId
-INNER JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId
-WHERE playlists.Name = 'TV Shows'
-LIMIT 100;
+SELECT UNIQUE Title AS AlbumName, Name AS TrackName, Milliseconds
+FROM albums
+LEFT JOIN tracks ON albums.AlbumId = tracks.AlbumId
+ORDER BY Milliseconds ASC
+LIMIT 50;
