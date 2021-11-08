0ef028ba40bf5235f4c9c9d658bea329dbda565c
Jcloux <joseph.cloux@ynov.com>
2021-11-08 10:57:05 +0100

    v1

---
+++ update-employee.sql
@@ -0,0 +1,9 @@
+-- Employee Robert King just got a promotion !
+-- You must update his function in the table employees.
+
+-- His new job Title : IT Manager
+-- His new superior : Adams Andrew
+
+UPDATE employees
+SET Title = 'IT Manager', ReportsTo = '1'
+WHERE "EmployeeId" = 7;
