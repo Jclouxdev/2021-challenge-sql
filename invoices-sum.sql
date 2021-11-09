a1436f5e95e7974eb20ce962010367d9bdc1cefc
Jcloux <joseph.cloux@ynov.com>
2021-11-09 10:54:11 +0100

    test com

--- invoices-sum.sql
+++ invoices-sum.sql
@@ -1,7 +1,3 @@
--- You have to get invoices information about invoices from client Tim Goyer. 
--- You must calculate the total sum of all his invoices 
--- with an alias AllInvoicesTotalPrice.
-
 SELECT SUM("Total") AllInvoicesTotalPrice 
 FROM invoices
 WHERE CustomerId = 19
