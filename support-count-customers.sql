6cb57efdfbad16ccbbcc997e915dc49385e81709
Jcloux <joseph.cloux@ynov.com>
2021-11-15 09:22:08 +0100

    v1

---
+++ support-count-customers.sql
@@ -0,0 +1,6 @@
+SELECT employees.FirstName || " " || UPPER(employees.LastName) AS FullName, COUNT(customers.CustomerId) AS "NumberOfCustomers"
+FROM customers
+INNER JOIN employees ON customers.SupportRepId = employees.EmployeeId
+WHERE employees.Title = "Sales Support Agent"
+GROUP BY customers.SupportRepId
+ORDER BY NumberOfCustomers
