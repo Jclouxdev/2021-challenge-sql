66c1b690985086e087844f97d7ab15ed6f0b5143
Jcloux <joseph.cloux@ynov.com>
2021-11-15 09:35:39 +0100

    test '

--- support-count-customers.sql
+++ support-count-customers.sql
@@ -5,7 +5,7 @@ FROM
 	customers
 	INNER JOIN employees ON customers.SupportRepId = employees.EmployeeId
 WHERE
-	employees.Title = "Sales Support Agent"
+	employees.Title = 'Sales Support Agent'
 GROUP BY
 	customers.EmployeeId
 ORDER BY
