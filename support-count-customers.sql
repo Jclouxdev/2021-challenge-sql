830ff7f7fc5ed9aef833dc75a766e02e19892c77
Jcloux <joseph.cloux@ynov.com>
2021-11-15 09:28:29 +0100

    test

--- support-count-customers.sql
+++ support-count-customers.sql
@@ -1,7 +1,12 @@
-SELECT employees.FirstName || " " || UPPER(employees.LastName) AS FullName, COUNT(customers.CustomerId) AS "NumberOfCustomers"
-FROM customers
-INNER JOIN employees ON customers.SupportRepId = employees.EmployeeId
-WHERE employees.Title = "Sales Support Agent"
-GROUP BY customers.SupportRepId
-ORDER BY NumberOfCustomers
---Commitchange
+SELECT
+	employees.FirstName || " " || UPPER(employees.LastName) AS FullName,
+	COUNT(customers.CustomerId) AS "NumberOfCustomers"
+FROM
+	customers
+	INNER JOIN employees ON customers.SupportRepId = employees.EmployeeId
+WHERE
+	employees.Title = "Sales Support Agent"
+GROUP BY
+	customers.SupportRepId
+ORDER BY
+	NumberOfCustomers
