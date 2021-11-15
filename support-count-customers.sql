4d7baf71f196cb1acec187483cd614aa122cbbbf
Jcloux <joseph.cloux@ynov.com>
2021-11-15 09:38:26 +0100

    test

--- support-count-customers.sql
+++ support-count-customers.sql
@@ -1,12 +1,12 @@
 SELECT
-	employees.FirstName || " " || UPPER(employees.LastName) FullName,
-	COUNT(customers.CustomerId) NumberOfCustomers
+	employees.FirstName || " " || UPPER(employees.LastName) as FullName,
+	COUNT(customers.CustomerId) as NumberOfCustomers
 FROM
 	customers
 	INNER JOIN employees ON customers.SupportRepId = employees.EmployeeId
 WHERE
 	employees.Title = 'Sales Support Agent'
 GROUP BY
-	customers.EmployeeId
+	customers.SupportRepId
 ORDER BY
 	NumberOfCustomers
